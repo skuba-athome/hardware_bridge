299fbe680c3fd4154365641bd14f7d0154d4a3c9
krit <krit@localhost>
2015-06-02 21:16:04 +0700

    Fix bug : mismatch topic type

--- nodes/joint_state_publisher.py
+++ nodes/joint_state_publisher.py
@@ -1,30 +1,45 @@
 #! /usr/bin/python
 import rospy
 from dynamixel_msgs.msg import JointState
+from sensor_msgs.msg import JointState as SensorJointState
 
 
-class JointStatePublisher:
-    def __init__(self, state_subcribe_topic, state_publisher_topic):
-        self.publisher = rospy.Publisher(state_publisher_topic, JointState)
-        rospy.Subscriber(state_subcribe_topic, JointState, self.state_callback)
-
-    def state_callback(self, joint_state):
-        self.publisher.publish(joint_state)
-
-
-class LumyaiJointPublisher:
+class JointStatesPublisher:
     def __init__(self):
         rospy.init_node('joint_states_publisher')
 
         motors_list = rospy.get_param('~motors_list')
         joint_state_publisher_topic = rospy.get_param('joint_state_topic', '/dynamixel/joint_states')
 
-        publisher_list = []
+        self.joint_state = {}
         for motor in motors_list:
             joint_state_topic = '/dynamixel/' + motor + '/state'
-            publisher = JointStatePublisher(joint_state_topic, joint_state_publisher_topic)
-            publisher_list.append(publisher)
-        rospy.spin()
+            rospy.Subscriber(joint_state_topic, JointState, self.state_callback)
+
+        self.publisher = rospy.Publisher(joint_state_publisher_topic, SensorJointState)
+    
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            self.publish_joint_states()
+            rate.sleep()
+
+    def state_callback(self, joint_state):
+        self.joint_state[joint_state.name] = joint_state
+
+    def publish_joint_states(self):
+        joint_states = SensorJointState()
+        joint_states.header.stamp = rospy.Time.now()
+
+        for joint in self.joint_state.values():
+            joint_states.name.append(joint.name)
+            joint_states.position.append(joint.current_pos)
+            #joint_states.velocity.append(joint.velocity)
+            #joint_states.effort.append(joint.load)
+
+        joint_states.name.append('torso_joint')
+        joint_states.position.append(0.0)
+
+        self.publisher.publish(joint_states)
         
 if __name__ == '__main__':
-    LumyaiJointPublisher()
+    JointStatesPublisher()
