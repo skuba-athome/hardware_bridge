c3df02d4e8dd20acd00903b84dc10fe6f062a41f
John Doe <john@doe.org>
2016-08-06 11:30:46 +0700

    update left_arm_joint_controller,right_Arm_joint_controller.yaml , base_connect, open_kinect_webcam.launch and odometry.py

--- nodes/odometry_est.py
+++ nodes/odometry_est.py
@@ -5,7 +5,7 @@ import tf
 
 from math import sin, cos, pi
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import TwistStamped, TwistWithCovarianceStamped, Quaternion, Pose2D, TransformStamped
+from geometry_msgs.msg import TwistStamped, TwistWithCovarianceStamped, Quaternion, Pose2D, TransformStamped, PoseWithCovarianceStamped
 
 class OdometryEstimation(object):
     def __init__(self):
@@ -21,8 +21,22 @@ class OdometryEstimation(object):
         self.OdometryTransformBroadcaster = tf.TransformBroadcaster()
         self.pose_odom = rospy.Publisher("odom", Odometry, queue_size=1)
         rospy.Subscriber("base_vel", TwistWithCovarianceStamped, self.velOdom_callback)
+        rospy.Subscriber("/robot_pose_ekf/odom_combined", PoseWithCovarianceStamped, self.odom_combined_callback)
         rospy.spin()
                 
+    def odom_combined_callback(self, odomPose):
+    	print 1111
+    	self.OdometryTransformBroadcaster.sendTransform(
+            (odomPose.pose.pose.position.x, odomPose.pose.pose.position.y, 0),
+            (odomPose.pose.pose.orientation.x, odomPose.pose.pose.orientation.y, odomPose.pose.pose.orientation.z, odomPose.pose.pose.orientation.w),
+            #tf.transformations.quaternion_from_euler(0, 0, self.odomPose.theta),
+            odomPose.header.stamp,
+            #rospy.Time.now(),
+            "base_link",
+            "odom"
+       	)
+
+
     def velOdom_callback(self,vel_odom):
         timenow = vel_odom.header.stamp
         dt = (timenow - self.last_est_time).to_sec()
@@ -51,32 +65,44 @@ class OdometryEstimation(object):
         odometry.pose.pose.position.y = self.odomPose.y
         odometry.pose.pose.position.z = 0
         odometry.pose.pose.orientation = quaternion
+        odometry.pose.covariance = [0.01,  0.0,  0.0,  0.0,  0.0,  0.0,
+                                    0.0,  0.01,  0.0,  0.0,  0.0,  0.0,
+                                    0.0,   0.0, 0.01,  0.0,  0.0,  0.0,
+                                    0.0,   0.0,  0.0,  0.1,  0.0,  0.0,
+                                    0.0,   0.0,  0.0,  0.0,  0.1,  0.0,
+                                    0.0,   0.0,  0.0,  0.0,  0.0,  0.1]
 
         odometry.child_frame_id = "base_link"
         odometry.twist.twist.linear.x = self.vx_odom
         odometry.twist.twist.linear.y = self.vy_odom
         odometry.twist.twist.angular.z = self.w_odom
+        odometry.twist.covariance = [0.01,  0.0,  0.0,  0.0,  0.0,  0.0,
+                                    0.0,  0.01,  0.0,  0.0,  0.0,  0.0,
+                                    0.0,   0.0, 0.01,  0.0,  0.0,  0.0,
+                                    0.0,   0.0,  0.0,  0.1,  0.0,  0.0,
+                                    0.0,   0.0,  0.0,  0.0,  0.1,  0.0,
+                                    0.0,   0.0,  0.0,  0.0,  0.0,  0.1]
         self.pose_odom.publish(odometry)
         
 #---------------------------------------------------------------------------------#
-        self.OdometryTransformBroadcaster.sendTransform(
-            (self.odomPose.x, self.odomPose.y, 0),
-            q,
-            #tf.transformations.quaternion_from_euler(0, 0, self.odomPose.theta),
-            timenow,
-            #rospy.Time.now(),
-            "base_link",
-            "odom"
-        )
-        self.OdometryTransformBroadcaster.sendTransform(
-            (0, 0, 0),
-            q,
-            #tf.transformations.quaternion_from_euler(0, 0, self.odomPose.theta),
-            timenow,
-            #rospy.Time.now(),
-            "odom",
-            "odom_combined"
-        )
+        #self.OdometryTransformBroadcaster.sendTransform(
+        #    (self.odomPose.x, self.odomPose.y, 0),
+        #    q,
+        #    #tf.transformations.quaternion_from_euler(0, 0, self.odomPose.theta),
+        #    timenow,
+        #    #rospy.Time.now(),
+        #    "base_link",
+        #    "odom"
+        #)
+        #self.OdometryTransformBroadcaster.sendTransform(
+        #    (0, 0, 0),
+        #    q,
+        #    #tf.transformations.quaternion_from_euler(0, 0, self.odomPose.theta),
+        #    timenow,
+        #    #rospy.Time.now(),
+        #    "odom",
+        #    "odom_combined"
+        #)
         self.last_est_time = timenow
 
 if __name__ == '__main__':
