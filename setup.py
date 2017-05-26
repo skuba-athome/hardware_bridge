7feb7b58b6afa46eeb878c0d64a726868e04565f
Frank <thanachot7@gmail.com>
2017-05-26 16:40:23 +0700

    Add udev rules

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+from subprocess import call
+import getpass
+import roslib
+
+# add user to dialout group
+username = getpass.getuser()
+call(["sudo", "adduser", username, 'dialout'])
+
+# copy rule to udev folder
+hardware_bridge_path = roslib.packages.get_pkg_dir('hardware_bridge')
+src = "{0}/99-skuba-usb.rules".format(hardware_bridge_path)
+dest = "/etc/udev/rules.d/99-skuba-usb.rules"
+call(["sudo", "cp", src, dest])
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
